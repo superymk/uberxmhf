ac330929f56386b56c19f674f82dcf0141669791
Eric Li <xiaoyili@andrew.cmu.edu>
2022-02-20 17:24:47 -0500

    reduce

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-baseplatform/arch/x86_64/bplt-x86_64-acpi.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-baseplatform/arch/x86_64/bplt-x86_64-acpi.c
@@ -92,13 +92,10 @@ static u32 _acpi_computetablechecksum(uintptr_t spaddr, uintptr_t size){
 
   p=(char *)spaddr;
   for(i=0; i< size; i++) {
-    for (int i = 0; i < 1000; i++) { udelay(1000); }
-    // TODO: add wbinvd here
     checksum+= (char)(*(p+i));
   }
   
   printf("\nDONE");
-  for (int i = 0; i < 1000; i++) { udelay(1000); }
   
   return (u32)checksum;
 }
@@ -125,8 +122,6 @@ u32 xmhf_baseplatform_arch_x86_64_acpi_getRSDP(ACPI_RSDP *rsdp){
     xmhf_baseplatform_arch_flat_copy((u8 *)rsdp, (u8 *)(ebdaphys+i), sizeof(ACPI_RSDP));
     if(rsdp->signature == ACPI_RSDP_SIGNATURE){
       if(!_acpi_computetablechecksum((uintptr_t)rsdp, 20)){
-        printf("\nFOUND 1");
-        for (int i = 0; i < 1000; i++) { udelay(1000); }
         found=1;
         break;
       }
@@ -142,11 +137,6 @@ u32 xmhf_baseplatform_arch_x86_64_acpi_getRSDP(ACPI_RSDP *rsdp){
     xmhf_baseplatform_arch_flat_copy((u8 *)rsdp, (u8 *)i, sizeof(ACPI_RSDP));
     //printf("\nLINE %d 0x%08lx 0x%08lx", __LINE__, (uintptr_t)rsdp, (uintptr_t)i);
     if(rsdp->signature == ACPI_RSDP_SIGNATURE){
-      printf("\nFILE:LINE %s:%d", __FILE__, __LINE__);
-      printf("\nWBINVD ..."); asm volatile ("wbinvd"); printf("done");
-      printf("\nWRITE CR3 ..."); write_cr3(read_cr3()); printf("done");
-      printf("\ni = 0x%016lx", i);
-      printf("\nFILE:LINE %s:%d", __FILE__, __LINE__);
       printf("\nWBINVD ..."); asm volatile ("wbinvd"); printf("done");
       printf("\nWRITE CR3 ..."); write_cr3(read_cr3()); printf("done");
       for (int i = 0; i < 1000; i++) { udelay(1000); }
